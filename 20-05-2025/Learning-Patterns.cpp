1dedec36701cf29f114948c51772f33a1e8d6782
ypranay3232 <ypranay56@gmail.com>
2025-05-20 14:20:20 +0530

    solid rectangle pattern learnt

--- 20-05-2025/Learning-Patterns.cpp
+++ 20-05-2025/Learning-Patterns.cpp
@@ -1,5 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-    
+    //full square pattern :
+    for(int row = 0; row<3; row = row+1){
+        for(int col=0;col<8;col = col +1){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
 }
