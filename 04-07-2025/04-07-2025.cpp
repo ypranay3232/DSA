3e89dd07b97898bff47ac02b24eb3fb10217a3dc
ypranay3232 <ypranay56@gmail.com>
2025-07-06 19:14:02 +0530

    revising everything i know

---
+++ 04-07-2025/04-07-2025.cpp
@@ -0,0 +1,18 @@
+// just want to practice the learnings.
+#include<iostream>
+using namespace std;
+
+int main(){
+    // lets see how to calculate a product of two numbers
+    int First_number ,Second_number;
+    int product;
+    cout<<"Enter 'First_number' value : "<<endl;
+    cin>>First_number;
+    cout<<"Enter 'Second_number' Value"<<endl;
+    cin>>Second_number;
+
+    product = First_number * Second_number;
+
+    cout<<"The product of both First and second numbers are : "<<product<<endl;
+
+}
