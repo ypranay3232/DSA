77c700dc7b933714866f2fd7246a3298b3bf87fc
ypranay3232 <ypranay56@gmail.com>
2025-10-10 22:43:07 +0530

    Revision

--- Learning_from_a_course/Day9-Patterns/18-09-25.cpp
+++ Learning_from_a_course/Day9-Patterns/18-09-25.cpp
@@ -16,6 +16,8 @@ int main()
     // working principle : works as first enters the outer loop (rows) and checks condition which is true then checks inner loop (columns) and now prints a star until 10(*********) now we print in new line with a endl statement.
     // now again checks rows = 1 and cols which are 0 because we are in a new line and prints 1- stars and break the line  and enter new line and goes on until rows become 6.
 
+
+    
     // Patterns Question 2
     // How to print Hollow square Pattern (A Square pattern with only boundaries)
     //  how we do is first we create two loops (outer and inner) then we add a if condition where we skip the stars
