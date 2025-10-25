fe5cecc29006abfaf1c57e9e3e912810989f97e4
ypranay3232 <ypranay56@gmail.com>
2025-10-25 07:02:49 +0530

    solving a new exercise

---
+++ Learning_from_a_course/Day15-More_paterns/25-10-2025.cpp
@@ -0,0 +1,17 @@
+// LEts see how to print this pattern ! 
+
+/*
+
+                    * * * * * * * * * * 
+                    * * * *     * * * *
+                    * * *         * * * 
+                    * *             * *
+                    *                 *
+                    *                 *
+                    * *             * *
+                    * * *         * * *
+                    * * * *     * * * *
+                    * * * * * * * * * *
+                    
+First we break the pattern into 3 parts (a left and right half pyramid and a empty spaces of a full pyramid) same for bottom 
+*/
