a84f52c3bb20048806cb43aa97c075cbf1e592e5
ypranay3232 <ypranay56@gmail.com>
2025-10-26 06:32:02 +0530

    25-10-2025 : learnt how to print a flipped solid half pattern and now trying to make it full

--- Learning_from_a_course/Day15-More_paterns/25-10-2025.cpp
+++ Learning_from_a_course/Day15-More_paterns/25-10-2025.cpp
@@ -1,4 +1,4 @@
-// LEts see how to print this pattern ! 
+// LEts see how to print the pattern Flipped SOlid Diamond ! 
 
 /*
 
@@ -13,5 +13,67 @@
                     * * * *     * * * *
                     * * * * * * * * * *
                     
-First we break the pattern into 3 parts (a left and right half pyramid and a empty spaces of a full pyramid) same for bottom 
+First we break the pattern into 3 parts (a left and right half pyramid and a empty spaces of a full pyramid) same for bottom. 
+
+Then we can see it's nothing more than a two half pyramids and a full pyramid of sapces
+
+Lets see how we gonna print it ! (As we can see the pattern ..! )
+
+rows   stars
+0       4
+1       3
+2       2
+3       1
+4       0
+
+
+// Now lets see the formula as we can observe (Assume : N =4) so N-row
+double check if it satisfy all the conditions ! 
+
+4-0 = 4
+4-1 = 3
+4-2 = 2
+4-3 = 1
+4-4 = 0
+
+
 */
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the height of the Pyramid : "<<endl;
+    cin>>n;
+    // First half pyramid 
+    for(int row=0;row<n;row=row+1){
+        // Half pyramid
+        for(int cols=0;cols<n-row;cols=cols+1){
+            cout<<"*";
+        }
+
+        // spaces 
+        for(int cols=0;cols<2*row;cols=cols+1){
+            cout<<" ";
+        }
+
+        // half pyramid
+        for(int cols=0;cols<n-row;cols=cols+1){
+            cout<<"*";
+        }
+        cout<<endl;
+    }//end of first half pyramid
+
+    // seond half pyrmaid
+    for(int row=0;row<n;row=row+1){
+        // first lets print the first half
+
+        /* so lets see how to get the pyramid as */
+
+        for(int cols=0;cols<2*row+1;cols=cols+1){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
