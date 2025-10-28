662d16199492f5d94fa3cd879abfe41d31c86d33
ypranay3232 <ypranay56@gmail.com>
2025-10-28 05:49:23 +0530

    28-10-2025 : More Patterns

---
+++ Learning_from_a_course/Day17-Patterns/28-10-2025-More_patterns.cpp
@@ -0,0 +1,52 @@
+/*
+Let's print this pattern : Hollow half Pyramid
+
+1 2 3 4 5
+2     5
+3   5
+4 5
+5
+
+Let's find the formula first : as we can see first we need to print row ==0,
+
+row     pattern(numbers)
+1           1
+2           2
+3           3
+4           4
+5           5
+
+so we can say that (row == cols). Now let's find out how to print the (remiaing n-1) pattern !
+rows  pattern(Numbers)
+1       5
+2
+3
+4
+5
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter the Length of the Pattern " << endl;
+    cin >> n;
+
+    for(int row = 1; row <= n; row = row + 1){
+        // The Pattern stars at 1 so we take row and cols = 1. 
+        for(int cols = 1; cols <= n; cols = cols + 1){
+            // same Print the cols and rows till they are equal to n. And the condition is : if row ==1 (first row), cols == row(Prints cols according to equal to row) so we get (12345) in both directions at row and cols
+
+            // Prints 5 at each last column so we use cols == n( cols 0,1,2,3,4 == 5) so when we print cols we print them as (2 5), (3 5), (4 5) at their respective positions so it looks like a (N-1) decrementing pattern for 5.
+            if(row == 1 || cols == row || cols == n){
+                cout << cols << " ";
+                // The else if prints two spaces inbetween the diagonals : cols > row == prints after diagonal (i.e 5), cols<n prits before diagonal (i.e 5)
+            } else if(cols > row && cols < n) {
+                cout << "  ";  // Only print spaces BETWEEN the diagonal and last column
+            }
+        }
+        cout << endl;
+    }
+}
