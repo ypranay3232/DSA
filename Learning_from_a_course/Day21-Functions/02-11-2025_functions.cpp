4d08b476fa3175cd22a474e05a1cfb792c56457b
ypranay3232 <ypranay56@gmail.com>
2025-11-02 07:07:11 +0530

    Functions

--- Learning_from_a_course/Day21-Functions/02-11-2025_functions.cpp
+++ Learning_from_a_course/Day21-Functions/02-11-2025_functions.cpp
@@ -1,4 +1,4 @@
-// In Genearl a function is a block of code designed to perform a specific task and you can re use it any where.
+// In Genearl a function is a block of code designed to perform a specific task and you can re use it any where in the program and can be used 'N' no of times
 
 // Assume that i want to print a statement 5 times, what can we do ? we can use loops ? yes let's see
 
@@ -15,6 +15,13 @@ void Repeat_and_reuse_ex() // Here in the braces we declare input parameters whi
     {
         cout << "Hello world" << endl;
     }
+}
+
+// Lets do an experiment we want to see what happens if i add return 1 or 2 or 3 etc. will it works ?  
+int hello()
+{
+    cout << "Hello " << endl;
+    return 2;//Yes it works even if we add return1,2,3,4,5,6.... etc
 }
 int main()
 {
@@ -34,13 +41,15 @@ int main()
 
             // Body of the function
 
-            
+
             Return 0;
         }
 
-        What is return 0 --> it indicates that the program is successfully executed without errors. (Normal termination) but if we return any non zero values such as 'Return 1' --> The os checks the script/code to detect it. 
-        
-        
+        What is return 0 --> it indicates that the program is successfully executed without errors. (Normal termination) but if we return any non zero values such as 'Return 1' --> The os checks the script/code to detect it.
+
         Simply the return 0 --> Works as an error flag (if 0 all fine if set to 1 or something os somes into picture and try to fix it)
     */
+
+    hello();
+    hello();
 }
