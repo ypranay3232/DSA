b5a8b5abbea9e0f48e334bcc49f52aff747af19e
ypranay3232 <ypranay56@gmail.com>
2025-11-03 05:36:42 +0530

    More functions

---
+++ Learning_from_a_course/Day22-Functions/3-11-2025_functions.cpp
@@ -0,0 +1,17 @@
+// FUnction call stack : A stack is a datastructure That follows last in first our order(LIFO)
+
+/* By using function call stack : It displays 
+Function call
+The function calls made by other functions
+Functions Local Variables
+function return type
+
+
+-----------------------------------
+In C++ Main() function gets executed first. And it contains a function called calculate()
+and the Main() calls calculate() and inside the calculate it contains a for loop and it gets executed and bring result back to main().
+------------------------------------
+
+Once any function is executed it gets poped() form the stack. And at last The main() function returns 0 and gets poped()
+
+*/
