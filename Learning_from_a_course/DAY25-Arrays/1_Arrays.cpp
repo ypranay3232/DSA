c4bef8daa8e5750e122fb6d9892eaa49e72cbdad
ypranay3232 <ypranay56@gmail.com>
2025-11-06 07:33:22 +0530

    .

--- Learning_from_a_course/DAY25-Arrays/1_Arrays.cpp
+++ Learning_from_a_course/DAY25-Arrays/1_Arrays.cpp
@@ -6,7 +6,8 @@ Integer size = 4 Bytes so it takes 4*5 = 20 bytes, And it stores in continous me
 
 Why we need Arrays ?
 
-Assume that we want to find the maximum of 3 numbers we declare and do a if else condition but what about 300,3000,30,000 etc. (.i.e. as the input numbers increases we need a way to store them so we use arrays)
+Assume that we want to find the maximum of 3 numbers we declare and do a if else condition but what 
+// about 300,3000,30,000 etc. (.i.e. as the input numbers increases we need a way to store so we use arrays)
 
 Ex: int array[30000];
 This creates a memory space of 30,000(we made 30,000 memory space without declaring the variables)
@@ -33,19 +34,24 @@ int main()
     int arr1[53];
     cout << "The integer array is : " << arr1 << endl;
 
-    // Why this Doesn't print anything is because for character : Cout has a special overload (char*) it assumes it's a c-string. It assumes to print every character in memory until it finds a null terminator('\0')
-    // Because the array is uninitialized (not assigned any values) it contains garbage values (so garbage value may be found out as 0X3491 so it stopped printing)
+    // Why this Doesn't print anything is because for character : Cout has a special overload (char*)
+    // it assumes it's a c-string. It assumes to print every character in memory until it finds a
+    //  null terminator('\0') Because the array is uninitialized (not assigned any values)
+    //  it contains garbage values (so garbage value may be found out as 0X3491 so it stopped printing)
 
-    // (.i.e. when a char aray is passed it points to char* pointer the ostream overload for char* treats it as a null terminated ('\0') c-string. since array is uninitialized it contains garbage values so it prints nothing)
+    // (.i.e. when a char aray is passed it points to char* pointer the ostream overload 
+    // for char* treats it as a null terminated ('\0') c-string. since array is uninitialized 
+    // it contains garbage values so it prints nothing)
     char array2[166] = {};
     // for verification we see the address if its starting with 0 then it's not going to print
-    cout << "The address of char is : " << &array2 << "Because its starting with 0 its not going to print" << endl;
+    cout << "The address of char is : " << &array2 << "Because its starting with 0, not going to print" << endl;
     cout << "The character array is : " << array2 << endl;
 
     bool array3[23] = {};
     cout << "The Boolean array is : " << array3 << endl;
 
-    // Array initializatin : can be done in two ways 1)create an array and dynamically add values to it 2)define an array ize and add values according to the size.
+    // Array initializatin : can be done in two ways 1)create an array and dynamically add values to it
+    //  2)define an array ize and add values according to the size.
     int Arr_ini[] = {1, 2, 3, 4, 5, 6, 7};
     cout << "Array initialized " << Arr_ini << endl;
 
@@ -76,10 +82,11 @@ int main()
     This is how the address is calculated in memory.    */
 
     int arr_init[] = {1, 2, 3};
-    cout <<"The array value is : "<< arr_init[0] << endl;
+    cout << "The array value is : " << arr_init[0] << endl;
     // Now printing all values from an array we use loop
-    for(int i =0;i<3;i++){
-        cout<<arr_init[i]<<" ";
+    for (int i = 0; i < 3; i++)
+    {
+        cout << arr_init[i] << " ";
     }
 
     return 0;
