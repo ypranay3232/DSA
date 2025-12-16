a39d1fd75b90d117774d6ac0690781d4a7de0c93
ypranay3232 <ypranay56@gmail.com>
2025-12-16 07:32:48 +0530

    Optimised solution for EKO Spoj

--- Learning_from_a_course/Day47_EKO_Spoj/1_eko_spoj.cpp
+++ Learning_from_a_course/Day47_EKO_Spoj/1_eko_spoj.cpp
@@ -6,7 +6,27 @@ jack need to chop down m meters of wood. It's an easy job for him because he has
 Jack machine works as :
 Jack set a height (H)in meters , then machine raises the sawblade of that height and cut the trees higher than the sawblade height.
 
-jack takes the cut of parts.
+jack takes the chopped wood ex: A Tree row contains [20,15,10,17] sawblade raised to 15meters
+so chopped wood will be 20,15,17. Because sawblade can cut more than 15 meters.
+
+
+Jack cares about environment so he wont cut more wood than necessary
+
+so Find maximum height of sawblade that allows jack to cut atleast m meter of wood(how much he need)
+
+ASSUME :
+
+Jack needs wood (M) = 7 meters
+He raised sawblade to (H) = 15  meters
+
+Trees are = 20,15,10,17
+
+since he cuts trees with a 15m height blade he gets wood as :
+(20-15) + (15-15) + (17-15) (ignore 10 became tree 10 is less than sawblade range (15))
+5+0+2 = 7meters
+
+jack got his wood of 7meters and saved environment without cutting whole tree
+
 
 
 1. Problem Statement :
@@ -51,10 +71,10 @@ Brute Force Idea :
 */
 #include <iostream>
 #include <vector>
-#include<algorithm>
+#include <algorithm>
 using namespace std;
 
-int ekoBruteForce(vector<int> &trees, int M)
+int ekoBruteForce(vector<long long> &trees, int M)
 {
 
     int maxHeight = *max_element(trees.begin(), trees.end());
@@ -70,7 +90,7 @@ int ekoBruteForce(vector<int> &trees, int M)
         for (int i = 0; i < trees.size(); i++)
         {
             // check if current tree is greater than height if yes cut else don't
-            
+
             if (trees[i] > H)
             {
                 woodCollected += (trees[i] - H);
@@ -87,14 +107,77 @@ int ekoBruteForce(vector<int> &trees, int M)
     return bestHeight;
 }
 
+// Optimised solution using Binary search :
+bool isoptimisedSolution(vector<long long> &inputTrees, long long int m, long long int mid)
+{
+    long long int woodcollected = 0;
+    for (int i = 0; i < inputTrees.size(); i++)
+    {
+        if (inputTrees[i] > mid)
+        {
+            woodcollected += inputTrees[i] - mid;
+        }
+    }
+    return woodcollected >= m;
+}
+
+long long int MaxSawBladeHeight(vector<long long> &inputTrees, long long int m)
+{
+    long long int start = 0, end, mid, ans = -1;
+    end = *max_element(inputTrees.begin(), inputTrees.end());
+
+    while (start <= end)
+    {
+        long long int mid = start + (end - start) / 2;
+        if (isoptimisedSolution(inputTrees, m, mid))
+        {
+            ans = mid;
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+    }
+    return ans;
+}
+
 int main()
 {
 
-    vector<int> trees = {20, 15, 10, 17};
-    int M = 7;
+    vector<long long> trees = {20, 15, 10, 17};
+    long long M = 7;
 
+    cout << "Brute Force Result:" << endl;
     cout << "Maximum cutting height = "
          << ekoBruteForce(trees, M) << endl;
 
+    // Binary search solution :
+      long long n, m;
+
+    cout << "Enter number of trees (N): ";
+    cin >> n;
+
+    cout << "minimum wood Jack required (M): ";
+    cin >> m;
+
+    vector<long long> inputTrees;
+
+    for (int i = 0; i < n; i++)
+    {
+        long long height;
+        cout << "Enter height of tree " << i + 1 << ": ";
+        cin >> height;
+        inputTrees.push_back(height);
+    }
+
+    cout << endl;
+    cout << "Optimized (Binary Search) Result:" << endl;
+    cout << "Maximum cutting height = "
+         << MaxSawBladeHeight(inputTrees, m) << endl;
+
+// No of trees = 4
+// minimum wood jack need is = 7
+// enter tree1,2,3,4 size = 20,15,10,17
     return 0;
 }
